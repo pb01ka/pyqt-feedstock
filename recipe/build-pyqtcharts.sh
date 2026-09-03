ed528321aae270c4c377bf250fea01b0b6bd8a11
pb01ka <gaganpb08singh@gmail.com>
2026-09-03 23:40:23 +0530

    Normalise CONDA_BUILD_CROSS_COMPILATION to "0"

--- recipe/build-pyqtcharts.sh
+++ recipe/build-pyqtcharts.sh
@@ -27,7 +27,9 @@ if [[ $(uname) == "Darwin" ]]; then
     export PATH=$PREFIX/bin/xc-avoidance:$PATH
 fi
 
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
+export CONDA_BUILD_CROSS_COMPILATION=${CONDA_BUILD_CROSS_COMPILATION:-0}
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]]; then
   SIP_COMMAND="$BUILD_PREFIX/bin/python -m sipbuild.tools.build"
   SITE_PKGS_PATH=$($PREFIX/bin/python -c 'import site;print(site.getsitepackages()[0])')
   EXTRA_FLAGS="--target-dir $SITE_PKGS_PATH"
